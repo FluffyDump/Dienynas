aa40b70082ddda3b1076664a3014843fe5269d80
Tomas <tomaspetrauskas50@gmail.com>
2024-11-10 20:37:21 +0200

    Implementuota vartotojų posistemės prototipas

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function show2fa() {
+    // Show the 2FA overlay when "Prisijungti" button is clicked
+    document.getElementById("2faOverlay").style.display = "block";
+}
 
-// Write your JavaScript code.
+function validate2fa(redirectUrl) {
+    // Check if there's any value entered in the 2FA code field
+    if (document.getElementById("2faCode").value.trim()) {
+        window.location.href = redirectUrl;
+    }
+}
+
+function showLogout() {
+    document.getElementById("logoutOverlay").style.display = "block";
+}
+
+function hideLogout() {
+    document.getElementById("logoutOverlay").style.display = "none";
+}
+
+function confirmLogout() {
+    window.location.href = '/Index';
+}
+
+function saveAndRedirect() {
+    // Perform any save actions here (if needed, in future logic)
+
+    // Redirect to profile view page
+    window.location.href = '/Profile';
+}
